42d4584a3693e9f8bf5d5c2ac22278cc9c13dcd5
tony <tonydrayton43@gmail.com>
2024-11-29 15:02:14 -0500

    Fix error logic for resume

--- src/app/api/getLatestResume/route.ts
+++ src/app/api/getLatestResume/route.ts
@@ -3,8 +3,7 @@ import { Routes, APIMessage } from 'discord-api-types/v9';
 
 const getLatestResume = async () => {
 	if (!process.env.DISCORD_BOT_TOKEN) {
-		console.error('DISCORD_BOT_TOKEN is not set');
-		return null;
+		throw new Error('DISCORD_BOT_TOKEN is not set');
 	}
 
 	const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_BOT_TOKEN);
@@ -17,23 +16,26 @@ const getLatestResume = async () => {
 }
 
 export async function GET(request: Request) {
-	const latestResumeUrl = await getLatestResume();
-	if (!latestResumeUrl) {
-		return new Response('Encountered an error or there was no PDF resume found.', { status: 404 });
+	try {
+		const latestResumeUrl = await getLatestResume();
+		if (!latestResumeUrl) {
+			return new Response('Encountered an error or there was no PDF resume found.', { status: 404 });
+		}
+		const pdfResponse = await fetch(latestResumeUrl);
+		if (!pdfResponse.ok) {
+			return new Response('Error fetching PDF from Discord.', { status: 500 });
+		}
+
+		const pdfBuffer = await pdfResponse.arrayBuffer();
+
+		return new Response(pdfBuffer, {
+			status: 200,
+			headers: {
+				'Content-Type': 'application/pdf',
+				'Content-Disposition': 'inline; filename="Tony_Drayton_Resume.pdf"'
+			},
+		});
+	} catch(err: any) {
+		return new Response(err.message, { status: 500 });
 	}
-
-	const pdfResponse = await fetch(latestResumeUrl);
-	if (!pdfResponse.ok) {
-		return new Response('Error fetching PDF from Discord.', { status: 500 });
-	}
-
-	const pdfBuffer = await pdfResponse.arrayBuffer();
-
-	return new Response(pdfBuffer, {
-		status: 200,
-		headers: {
-			'Content-Type': 'application/pdf',
-			'Content-Disposition': 'inline; filename="Tony_Drayton_Resume.pdf"'
-		},
-	});
 }
